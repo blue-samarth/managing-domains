67ea1a522db2c85484837d45ad879e0e8e91b2a5
Samarth <samarth38work@gmail.com>
2025-07-27 12:50:49 +0000

    Adding-provider.tf

---
+++ gcp/provider.tf
@@ -0,0 +1,22 @@
+locals {
+  name          = "<NAME_OF_YOUR_PROJECT>"
+  region        = "asia-south1"
+  zone         = "asia-south1-a"
+  organization_id = "<YOUR_ORG_ID>"
+  billing_account = "<YOUR_BILLING_ACCOUNT_ID>"
+
+  org_shorthand = "<ORG_SHORTHAND>"
+  environment   = "prod"
+
+
+  vpc_cidr = "10.0.0.0/16"                           # VPC CIDR block
+  azs      = ["list", "of", "availability", "zones"] # Availability Zones
+
+  cluster_version = "1.33" # EKS cluster version
+  namespace = ["production", "staging", "development"]
+
+  domain  = "example.com"    # Replace with your domain
+  zone_id = "<YOUR_ZONE_ID>" # Route53 zone ID for the domain
+
+  cloudflare_zone_id = "<YOUR_CLOUDFLARE_ZONE_ID>" # Cloudflare zone ID for the domain
+}
