d01fbc79c6b4f19cd525678fdc662bc2b0b61361
Samarth <samarth38work@gmail.com>
2025-07-27 10:49:03 +0000

    Adding-acm-service

---
+++ acm_service.tf
@@ -0,0 +1,35 @@
+module "acm_service" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 5.1.0"
+
+  domain_name = local.domain
+  zone_id     = local.zone_id
+
+  subject_alternative_names = [
+    "www.${local.domain}",
+    "api.${local.domain}",
+    "app.${local.domain}"
+  ]
+
+  create_route53_records  = false
+  validation_method       = "DNS"
+  validation_record_fqdns = cloudflare_dns_record.dns_record_validation[*].name
+
+  tags = merge(module.tags.tages, {
+    Name = local.domain
+  })
+}
+
+resource "cloudflare_dns_record" "dns_record_validation" {
+  count = length(module.acm_service.distinct_domain_names)
+
+  zone_id = local.cloudflare_zone_id
+  name    = trimsuffix(element(module.acm_service.validation_domains, count.index)["resource_record_name"], ".")
+  type    = element(module.acm_service.validation_domains, count.index)["resource_record_type"]
+  ttl     = 300
+
+  content = trimsuffix(element(module.acm_service.validation_domains, count.index)["resource_record_name"], ".")
+  comment = "ACM DNS validation for ${local.domain} - ${element(module.acm_service.validation_domains, count.index)["resource_record_type"]}"
+
+  proxied = false
+}
